2976893107eb25e227191a5a559e5146febba61f
TorlettiJoaquin <axeltorletti@gmail.com>
2023-08-23 15:08:45 -0300

    feat: add bg colors into tailwind presets

--- frontend/src/Pages/Login/Login.tsx
+++ frontend/src/Pages/Login/Login.tsx
@@ -1,10 +1,10 @@
 export const Login = () => {
   return (
-    <section className="min-h-screen flex items-stretch bg-[#f7f7f7]">
+    <section className="min-h-screen flex items-stretch bg-background">
       <div className="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0">
         {/* <div>En este div va a ir la imagen de la derecha nuevamente para suponer el login en la version mobile</div> */}
         <div className="w-full py-6 z-20">
-          <h1 className="text-[#13ADB7] text-6xl font-medium font pb-40">
+          <h1 className="text-primary text-6xl font-medium font pb-40 font-Ubuntu">
             RallyFund
           </h1>
           <p className="font-bold text-4xl ">¡Hola nuevamente!</p>
@@ -24,18 +24,18 @@ export const Login = () => {
               />
             </div>
             <div>
-              <p className="text-[#13ADB7] font-bold underline text-xs text-start">
+              <p className="text-primary font-bold underline text-xs text-start">
                 Recupera tu contraseña
               </p>
             </div>
             <div className="my-6">
-              <button className="w-full p-4 rounded-lg bg-[#13ADB7] text-white font-bold text-base">
+              <button className="w-full p-4 rounded-lg bg-primary hover:bg-hover transition-all duration-300 text-white font-bold text-base">
                 Iniciar sesión
               </button>
             </div>
             <div className="flex text-center justify-center gap-3 text-xs font-normal">
               <p>¿No tenes una cuenta?</p>
-              <p className="text-[#13ADB7] font-bold underline ">Registrate</p>
+              <p className="text-primary font-bold underline ">Registrate</p>
             </div>
           </form>
         </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,14 +3,22 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors:{
+      colors: {
         primary: '#13ADB7',
       },
-      fontFamily:{
+      backgroundColor: {
+        primary: '#13ADB7',
+        background: '#f7f7f7',
+        hover: '#108087',
+      },
+      fontFamily: {
         Ubuntu: ['Ubuntu', 'sans-serif'],
         Poppins: ['Poppins', 'sans-serif'],
-        Raleway : ['Raleway', 'sans-serif']
-      }
+        Raleway: ['Raleway', 'sans-serif'],
+      },
+      backgroundImage: {
+        'login-background': "url('/src/Pages/Login/login-background.png')",
+      },
     },
   },
   plugins: [],
