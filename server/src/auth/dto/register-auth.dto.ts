1f117d1d9eafae9a9559a85bd560eb61c7fe0de3
Santiago Volentiera <santiagovolentiera@gmail.com>
2023-08-24 13:09:57 -0300

    feat: login&Register

---
+++ server/src/auth/dto/register-auth.dto.ts
@@ -0,0 +1,54 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { IsOptional, IsString, IsBoolean, IsInt, MinLength, IsEmail, MaxLength } from 'class-validator';
+import { LoginAuthDto } from './login-auth.dto';
+
+export class RegisterAuthDto extends PartialType(LoginAuthDto) {
+  @IsEmail()
+  email: string;
+
+  @MinLength(6)
+  @MaxLength(20)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  postal_code?: string;
+
+  @IsOptional()
+  @IsString()
+  profile_picture?: string;
+
+  @IsOptional()
+  @IsString()
+  country?: string;
+
+  @IsOptional()
+  @IsString()
+  document_type?: string;
+
+  @IsOptional()
+  @IsInt()
+  document_number?: number;
+
+  @IsOptional()
+  @IsInt()
+  phone_number?: number;
+
+  @IsString()
+  first_name: string;
+
+  @IsString()
+  last_name: string;
+
+  @IsOptional()
+  @IsBoolean()
+  verified: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  is_active: boolean;
+}
