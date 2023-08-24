e3f9787ce0079157719960145f08598fbdadbdd5
Jose Elias Morales <joselias.dev@gmail.com>
2023-08-24 14:26:05 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <Route path="registerChoice" element={<RegisterChoice />} />
         </Route>
         <Route path="sendEmail" element={<EmailPage />} />
-        <Route path="/" />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
