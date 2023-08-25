8e99865d3cf02965f05d5d85cad297c84f5968e1
Joselias <joselias.dev@gmail.com>
2023-08-24 22:21:06 -0300

    fix: Fixed Pages folder name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EmailPage from './Pages/EmailPage.tsx';
-import { Login } from './Pages/Login/Login';
+import { Login } from './Pages/Login/Login.tsx';
 import { LayoutAuth } from './Layout/LayoutAuth';
-import { Register } from './Pages/Register/Register';
-import { RegisterChoice } from './Pages/Register/RegisterChoice';
-import { HomePage } from './Pages/HomePage';
+import { Register } from './Pages/Register/Register.tsx';
+import { RegisterChoice } from './Pages/Register/RegisterChoice.tsx';
+import { HomePage } from './Pages/HomePage.tsx';
 import { Navbar } from './components/layout';
 
 function App() {
