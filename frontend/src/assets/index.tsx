1d54fbcaf097c3cfcbb70689f22aed4fc1121b9d
Masiel <masiel31@msn.com>
2023-08-30 10:45:02 -0300

    Add:Cards, banner, buttonSection, review

--- frontend/src/assets/index.tsx
+++ frontend/src/assets/index.tsx
@@ -2,7 +2,15 @@ import phonePersonal from './phone-personal.png';
 import phoneTercero from './phone-terceros.png';
 import hand from './handsPoint.png';
 import home1 from './home1.png';
+import home2 from './home2.png';
+import home3 from './home3.png';
 import paso1 from './paso1.png';
 import paso2 from './paso2.png';
 import paso3 from './paso3.png';
 import saly from './saly.png';
+import imgExample from './imgExample.png';
+import landing from './LandingPage1.png';
+import similar from './ProyectosSimilares.png';
+
+
+export { phonePersonal, phoneTercero, hand,  paso1, paso2, paso3, saly, home1, home2, home3, imgExample, landing, similar };
--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -1,2 +1,6 @@
 export * from './Navbar';
 export * from './PopUpDonation';
+export * from './ButtonSectionHome';
+export * from './CardsHome';
+export * from './Hero';
+export * from './Step';
