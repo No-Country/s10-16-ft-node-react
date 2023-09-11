3f8194a2d167b925f362bc88a31cd8dd14de52b2
TorlettiJoaquin <axeltorletti@gmail.com>
2023-09-11 11:34:02 -0300

    feat: global state for filter and search

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -9,3 +9,23 @@ export const useBgStore = create<BgStoreState>()((set) => ({
   backgroundClass: 'bg-register-background',
   setBackgroundClass: (className) => set({ backgroundClass: className }),
 }));
+
+interface FilterStoreState {
+  filter: string;
+  setFilter: (filter: string) => void;
+}
+
+export const useFilterStore = create<FilterStoreState>()((set) => ({
+  filter: '',
+  setFilter: (filter) => set({ filter }),
+}));
+
+interface SearchStoreState {
+  search: string;
+  setSearch: (search: string) => void;
+}
+
+export const useSearchStore = create<SearchStoreState>()((set) => ({
+  search: '',
+  setSearch: (search) => set({ search }),
+}));
