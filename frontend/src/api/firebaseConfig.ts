ce3ab6652f7b5717fd751e1c58d4c754dca41e15
TorlettiJoaquin <axeltorletti@gmail.com>
2023-09-13 01:57:05 -0300

    feat: firebase config ready to use firestore

--- frontend/src/api/firebaseConfig.ts
+++ frontend/src/api/firebaseConfig.ts
@@ -1,16 +1,27 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASEAPP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASEAPP_ID,
+  apiKey: 'AIzaSyCXQNbnhkJXLuoUhfnkrfhPDVm8F517Oug',
+  authDomain: 'rallyfund-1b44f.firebaseapp.com',
+  projectId: 'rallyfund-1b44f',
+  storageBucket: 'rallyfund-1b44f.appspot.com',
+  messagingSenderId: '693560298400',
+  appId: '1:693560298400:web:cf23115ed345b7b6aeb2c5',
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const db = getFirestore(app);
+
+export default db;
